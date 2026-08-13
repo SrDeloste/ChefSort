a4be88bf276f609de09da3530d3c8d734ab54b29
Mr. Deloste <ezequieldeloste13@gmail.com>
2026-08-13 18:25:30 -0300

    Adiciona desafio Aventureiro comSelection Sort

---
+++ Desafios/Tema 1/aventureiro.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+struct Prato
+{
+    char nome[50];
+    int quantidadeIngredientes;
+};
+
+int main()
+{
+
+    struct Prato pratos[5] = {
+        {"Lasanha", 8},
+        {"Salada", 4},
+        {"Hamburguer", 6},
+        {"Omelete", 3},
+        {"Strogonoff", 7}};
+
+    printf("=== PRATOS ANTES DA ORDENACAO ===\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s - %d ingredientes\n",
+               pratos[i].nome,
+               pratos[i].quantidadeIngredientes);
+    }
+
+    // Selection Sort
+    for (int i = 0; i < 4; i++)
+    {
+
+        int min_idx = i;
+
+        for (int j = i + 1; j < 5; j++)
+        {
+
+            if (pratos[j].quantidadeIngredientes <
+                pratos[min_idx].quantidadeIngredientes)
+            {
+
+                min_idx = j;
+            }
+        }
+
+        if (min_idx != i)
+        {
+
+            struct Prato temp = pratos[i];
+            pratos[i] = pratos[min_idx];
+            pratos[min_idx] = temp;
+        }
+    }
+
+    printf("\n=== PRATOS DEPOIS DA ORDENACAO ===\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s - %d ingredientes\n",
+               pratos[i].nome,
+               pratos[i].quantidadeIngredientes);
+    }
+
+    return 0;
+}
