f64292fda21a81fcb300bf5b4a993300898df6bd
Mr. Deloste <144692172+SrDeloste@users.noreply.github.com>
2026-08-13 19:01:58 -0300

    Update aventureiro.c

--- Desafios/Tema 1/aventureiro.c
+++ Desafios/Tema 1/aventureiro.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-//criação da "ficha" do prato
+// criação da "ficha" do prato
 struct Prato
 {
-//criando o modelo do prato
+    // criando o modelo do prato
     char nome[50];
     int quantidadeIngredientes;
 };
 
 int main()
 {
-//Prato e quantidade de ingredientes
+    // Prato e quantidade de ingredientes
     struct Prato pratos[5] = {
         {"Lasanha", 8},
         {"Salada", 4},
@@ -18,23 +18,23 @@ int main()
         {"Strogonoff", 7}};
 
     printf("=== PRATOS ANTES DA ORDENACAO ===\n");
-//Apenas pra mostrar os pratos
+    // Apenas pra mostrar os pratos
     for (int i = 0; i < 5; i++)
     {
         printf("%s - %d ingredientes\n",
                pratos[i].nome,
                pratos[i].quantidadeIngredientes);
-    } 
+    }
 
     // Selection Sort
     for (int i = 0; i < 4; i++)
     {
-//i escolhe a posição → min_idx lembra o menor → j procura → if compara → troca.
+        // i escolhe a posição → min_idx lembra o menor → j procura → if compara → troca.
         int min_idx = i;
-        //Guarda temporariamente o prato com menos ingredientes na primeira verificação
+        // Guarda o índice do prato com menos ingredientes encontrado até agora
 
         for (int j = i + 1; j < 5; j++)
-//Procura o Menor
+        // Procura o Menor
         {
             if (pratos[j].quantidadeIngredientes <
                 pratos[min_idx].quantidadeIngredientes)
@@ -43,14 +43,14 @@ int main()
                 min_idx = j;
             }
         }
-
-        if (min_idx != i) //troca
+        // Compara a quantidade de ingredientes, mas movimenta o prato inteiro
+        if (min_idx != i) // troca
         {
 
-            struct Prato temp = pratos[i]; //Compara a quantidade, movimenta o prato inteiro.
+            struct Prato temp = pratos[i]; // Guarda temporariamente o prato atual para realizar a troca
             pratos[i] = pratos[min_idx];
             pratos[min_idx] = temp;
-        } //Variavel temporaria
+        } // Fim da troca
     }
 
     printf("\n=== PRATOS DEPOIS DA ORDENACAO ===\n");
